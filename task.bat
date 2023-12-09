fd7b9deea6a13b11347416b9325752127cbceded
Jepyhaja <jepyhaja@gmail.com>
2023-12-09 23:10:40 +0200

    M task.bat

--- task.bat
+++ task.bat
@@ -1,3 +1,3 @@
 @echo off > NUL
 cd D:\Obsidian
-cmd /s /c ""c:\Program Files\Git\bin\bash.exe" --login -i -- D:\Obsidian\sync.sh"
+cmd/s/c ""c:\Program Files\Git\bin\bash.exe" --login -i -- D:\Obsidian\sync.sh"
